cff2e5ffe3578960e5a0b015956371841f841035
asyncedd <isynqquwu@proton.me>
2023-10-04 17:51:29 +0900

    perf: Make the code more performant

--- main.c
+++ main.c
@@ -14,6 +14,12 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
   switch (key) {
     case ARGP_KEY_ARG:
+      arguments->dir_names = realloc(
+          arguments->dir_names, (arguments->num_dirs + 1) * sizeof(char *));
+      if (arguments->dir_names == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return ENOMEM;
+      }
       arguments->dir_names[arguments->num_dirs++] = arg;
       break;
     case ARGP_KEY_END:
@@ -33,11 +39,7 @@ int main(int argc, char *argv[]) {
   struct arguments arguments;
 
   arguments.num_dirs = 0;
-  arguments.dir_names = (char **)malloc(argc * sizeof(char *));
-  if (arguments.dir_names == NULL) {
-    fprintf(stderr, "Memory allocation failed.\n");
-    return 1;
-  }
+  arguments.dir_names = NULL;
 
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
